7bcfbb40c56096f91750d292dfcfe34ddb19deb3
SnowdenxEdward <snowdenxedward@gmail.com>
2024-10-02 23:12:40 +0530

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,14 @@
+const computer =  {
+     processor : "Intel Core i9-14900K",
+     GPU : "NVIDIA GeForce RTX 4090",
+     Motherboard :  "ASUS ROG Maximus Z790 Extreme",
+     RAM : "Corsair Vengeance DDR5 32GB (8 x 32GB) DDR5-6000" ,
+     SSD : "Samsung 980 PRO 2TB NVMe M.2 SSD",
+     Cooling : "Corsair H150i Elite Capellix",
+     PSU : "Corsair RM1000x",
+     Case : "Fractal Design Meshify 2",
+     Monitor : "ASUS ROG Swift OLED PG32UQX",
+     Mouse : "Logitech G Pro X Superlight",
+     Keyboard : "Corsair K100 RGB Mechanical Gaming Keyboard",
+}     
+console.log(computer);
